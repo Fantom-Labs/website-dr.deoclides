33fac6720ec338ff78ed9b9814c8cf2a0e7ada04
Fantom <fantom.weblabs@gmail.com>
2026-07-14 13:04:20 -0300

    🐛 fix: adiciona generateStaticParams na rota do studio para compatibilizar com cache components

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,6 +1,10 @@
 import StudioWrapper from "./StudioWrapper";
 export { metadata, viewport } from "next-sanity/studio";
 
+export function generateStaticParams() {
+  return [{ tool: [] }];
+}
+
 export default function StudioPage() {
   return <StudioWrapper />;
 }
