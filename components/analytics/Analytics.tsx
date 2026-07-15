474d64571ad66bafe03b08611f85b62bfdd27f41
Fantom <fantom.weblabs@gmail.com>
2026-07-15 00:42:20 -0300

    ✨ feature: adiciona suporte ao google tag manager (gtm) no analytics

--- components/analytics/Analytics.tsx
+++ components/analytics/Analytics.tsx
@@ -8,6 +8,7 @@ type Consent = "accepted" | "declined" | null;
 const GA4_ID = process.env.NEXT_PUBLIC_GA4_ID;
 const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID;
 const GOOGLE_ADS_ID = process.env.NEXT_PUBLIC_GOOGLE_ADS_ID;
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
 export default function Analytics() {
   const [consent, setConsent] = useState<Consent>(null);
@@ -28,6 +29,25 @@ export default function Analytics() {
 
   return (
     <>
+      {GTM_ID && (
+        <>
+          <Script id="gtm-init" strategy="afterInteractive">{`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}</Script>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        </>
+      )}
       {hasGtag && (
         <>
           <Script
